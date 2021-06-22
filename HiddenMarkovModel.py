dabf4e513c317c8d9e8aaf715f76c6b14cca157c
Chang Tzu Yen <zy.zhang1129@gmail.com>
2021-06-22 20:55:42 +0800

    update forward func
    更新了向前算法

--- HiddenMarkovModel.py
+++ HiddenMarkovModel.py
@@ -1,10 +1,12 @@
 import numpy as np
-import pandas as pd
 
 
-def foward(data, hidden_prob, visible_prob, init):
-    # 需要混淆矩阵，CNM Windows是真难用
-    prediction = np.zeros(hidden_prob.shape[0], data.shape[0])
-    for i in range(1, hidden_prob.shape[0]):
-        for j in range(visible_prob.shape[0]):
-            prediction[i, j] = prediction[i - 1].dot(hidden_prob[:, j]) * visible_prob[j, data[i]]
+def forward_algorithm(data, transition_prob, emission_prob, initial_distribution):
+    p = np.zero(data.shape[0], transition_prob[0])
+    b = emission_prob[:data.shape[0]]
+    p = initial_distribution * b
+    for t in range(1, data.shape[0]):
+        for i in range(0, transition_prob.shape[0]):
+            p[t, i] = p[t - 1].dot(p[:, i], b[i, t])
+    return p
+
